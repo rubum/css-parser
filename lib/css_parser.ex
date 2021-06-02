ed998d5eb7af47037b4b2d9bf8bd75384e5f6bd8
Nganga Mburu <mburunganga4@gmail.com>
2021-06-02 12:01:44 +0300

    initialize lib

---
+++ lib/css_parser.ex
@@ -0,0 +1,18 @@
+defmodule CssParser do
+  @moduledoc """
+  Documentation for `CssParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CssParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/css_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule CssParserTest do
+  use ExUnit.Case
+  doctest CssParser
+
+  test "greets the world" do
+    assert CssParser.hello() == :world
+  end
+end
