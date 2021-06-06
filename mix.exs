e3e4af33211503c951238c7a0268d3bd413ac38f
mburu kenneth <33229008+rubum@users.noreply.github.com>
2021-06-06 15:25:58 +0300

    Add metadata to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule CssParser.MixProject do
   use Mix.Project
+  
+  @source_url "https://github.com/rubum/css-parser"
+  @version "0.1.0"
 
   def project do
     [
       app: :css_parser,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+      
+      # Hex
+      description: "Provides css parsing in Elixir",
+      package: package(),
+
+      # Docs
+      name: "CssParser",
+      docs: docs()
     ]
   end
 
@@ -25,4 +36,23 @@ defmodule CssParser.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+  
+  defp package do
+    [
+      maintainers: ["Kenneth Mburu"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url},
+      files:
+        ~w(.formatter.exs mix.exs README.md lib test) ++
+        ~w(priv mix.exs README* readme* LICENSE* license* CHANGELOG* changelog* src),
+    ]
+  end
+  
+  defp docs do
+    [
+      main: "CssParser",
+      source_ref: "v#{@version}",
+      source_url: @source_url
+    ]
+  end 
 end
